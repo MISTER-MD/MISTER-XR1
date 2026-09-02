2c300d7cbdd6aac5af65fdade5fcd421fad4b5cc
MISTER-MD <rosyvannellc@gmail.com>
2026-09-02 19:42:24 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,38 @@
+module.exports = {
+// 🤖 BOT INFORMATION
+BOT_NAME: "MISTER XR",
+VERSION: "1.0.0",
+AUTHOR: "MISTER XR",
+
+// ⚙️ BOT SETTINGS
+PREFIX: ".",
+
+// 👑 OWNER
+OWNER_NAME: "MISTER XR",
+OWNER_NUMBER: "509XXXXXXXX",
+
+// 🌐 WEBSITE
+WEBSITE: "https://mister-xr.onrender.com",
+
+// 🖼️ MENU
+MENU_IMAGE: "./media/menu.jpg",
+
+// 👋 GROUP SETTINGS
+WELCOME: true,
+GOODBYE: true,
+
+// 🛡️ SECURITY
+ANTI_LINK: false,
+ANTI_SPAM: false,
+ANTI_BADWORD: false,
+
+// 🔧 OTHER
+AUTO_READ: false,
+AUTO_TYPING: false
+};
+module.exports = {
+    BOT_NAME: "MISTER XR",
+    PREFIX: ".",
+    OWNER_NAME: "MISTER XR",
+    CHANNEL_LINK: "https://whatsapp.com/channel/0029VbDXcQG3AzNQFP5ld22w",
+};
